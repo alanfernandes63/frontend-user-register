92aa85c7e6ca3284e8f9a55b6a0bb1868c1932e5
alan <alan.fernandes63@gmail.com>
2019-12-29 16:34:30 -0300

    melhorando a visualizacao em arvore do usuario

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -1,58 +1,39 @@
 import React from 'react';
 import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
-import { makeStyles } from '@material-ui/core/styles';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import TreeView from '@material-ui/lab/TreeView';
+import TreeItem from '@material-ui/lab/TreeItem';
+import ListStyle from './style'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
-const listStyle = makeStyles({
-  list:{
-    width:250,
-  }
-})
+const MakeList = () => {
 
-const list = () => {
-  const classes = listStyle()
+
+  const classes = ListStyle();
+
   return(
-<div className={ classes.list } role="presentation">
-  <List>
-    {
-      ['teste1','teste2'].map(
-          (text, index) => (
-          <ListItem button key={ text }>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={ text }></ListItemText>
-          </ListItem>
-          )
-      )
+    <div className={ classes.list } role="presentation">
 
-    }
-  </List>
-<Divider/>
-  <List>
-  {
-    ['teste1','teste2'].map(
-        (text, index) => (
-        <ListItem button key={ text }>
-          <ListItemText primary={ text }></ListItemText>
-        </ListItem>
-        )
-    )
+      {
+        <TreeView
+        defaultExpandIcon={ <ExpandMoreIcon /> }
+        defaultCollapseIcon={ <ChevronRightIcon /> }>
+          <TreeItem label="User">
+            <TreeItem label="add"></TreeItem>
+            <TreeItem label="list"></TreeItem>
+          </TreeItem>
+        </TreeView>
 
-  }
-  </List>
-  </div>
+      }
+
+      </div>
   )
 }
 
 export default function Mybutton(){
   return(
     <Drawer open={true}>
-      { list() }
+      { MakeList() }
     </Drawer>
   );
 }
---
+++ src/components/style.js
@@ -0,0 +1,11 @@
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const ListStyle = makeStyles(theme => ({
+    list:{
+      width:300,
+    },
+  }
+  ));
+
+  export default ListStyle;
